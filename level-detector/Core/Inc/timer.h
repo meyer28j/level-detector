4f0957249fb3a2fc8cb9c14f70338826191ee134
Jacob Meyer <meyer28j@uregina.ca>
2025-03-28 03:14:26 -0600

    Add doxygen comments for timer module

--- level-detector/Core/Inc/timer.h
+++ level-detector/Core/Inc/timer.h
@@ -1,8 +1,7 @@
-/*
- * timer.h
- *
- *  Created on: Nov 5, 2024
- *      Author: jacob
+/**
+ * @file timer.h
+ * @author Jacob Meyer
+ * @ brief This file contains the implementation of the timer functions.
  */
 
 #ifndef INC_TIMER_H_
@@ -10,11 +9,31 @@
 
 #include "stm32f1xx_hal.h"
 
+/**
+ * @def MAX_TIME
+ * @brief Maximum value for the timer counter.
+ *
+ * This constant defines the upper limit of the timer's 16-bit counter.
+ */
 #define MAX_TIME 0xFFFF
-/*
- * TIMER control functions
+
+/**
+ * @brief Starts the specified timer.
+ *
+ * This function initiates the interrupt-related timer counter.
+ * if configured.
+ *
+ * @param[in] htim The HAL global handler for TIM peripherals.
  */
 void timer_start(TIM_HandleTypeDef* htim);
+
+/**
+ * @brief Stops the specified timer.
+ *
+ * This function halts the interrupt-related timer counter.
+ *
+ * @param[in] htim The HAL global handler for TIM peripherals.
+ */
 void timer_stop(TIM_HandleTypeDef* htim);
 
 #endif /* INC_TIMER_H_ */
