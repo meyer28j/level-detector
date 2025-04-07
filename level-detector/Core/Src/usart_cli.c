7497541cf0c15d46a814aa07b4692cf19794994b
Jacob Meyer <meyer28j@uregina.ca>
2025-04-07 14:44:57 -0600

    Add cursor hiding to terminal update; cleanup RefreshStatus function

--- level-detector/Core/Src/usart_cli.c
+++ level-detector/Core/Src/usart_cli.c
@@ -14,7 +14,9 @@ static const char ANSI_SCROLL_WINDOW[] = {"\x1b[6;20r"};
 static const char ANSI_CLEAR_LINE[] = {"\033[2K"};
 static const char ANSI_SAVE_CURSOR_POS[] = {"\x1b[s"};
 static const char ANSI_RETURN_CURSOR_POS[] = {"\x1b[u"};
-static const char ANSI_MOVE_TO_STATUS_LINE[] = {"\x1b[1;0f"};
+static const char ANSI_MOVE_TO_STATUS_LINE[] = {"\x1b[H"};//{"\x1b[2;0f"};
+static const char ANSI_HIDE_CURSOR[] = {"\x1b[?25l"};
+static const char ANSI_SHOW_CURSOR[] = {"\x1b[?25h"};
 
 static const char INTRO_MSG[] = "**Level Detector Debug Terminal**\r\n";
 static const char HELP_MSG[] = "Commands:\r\n"
@@ -51,14 +53,17 @@ void RefreshStatus(UART_HandleTypeDef* huart)
 	// TODO: move this to be timer based
 	AccelData accel_values = read_accelerometer_data();
 
-	char message[MSG_LEN] = {'\0'};
-	char level[MSG_LEN] = {'\0'};
-	char z_axis[MSG_LEN] = {'\0'};
-	char y_axis[MSG_LEN] = {'\0'};
-	char x_axis[MSG_LEN] = {'\0'};
-	char z_ascii[20];
-	char y_ascii[20];
-	char x_ascii[20];
+	char message[MSG_LEN] = {'\0'}; 	// the final combined message to transmit
+	char level[64] = {'\0'}; 		// TRUE or FALSE based on x/y/z data
+	char z_axis[64] = {'\0'}; 		// displays message of each axis data
+	char y_axis[64] = {'\0'};
+	char x_axis[64] = {'\0'};
+	char z_ascii[20] = {'\0'}; 			// stores integer data as string
+	char y_ascii[20] = {'\0'};
+	char x_ascii[20] = {'\0'};
+	char move_to_status[64] = {'\0'};	// ANSI characters for moving to status line
+	char data[MSG_LEN] = {'\0'};		// combined messages for each data line
+	char move_to_cmd[64] = {'\0'};		// ANSI characters for returning to cmd> line
 
 	if (is_level(accel_values))
 	{
@@ -69,19 +74,40 @@ void RefreshStatus(UART_HandleTypeDef* huart)
 		snprintf(level, MSG_LEN, "%sLEVEL: %s\r\n", ANSI_CLEAR_LINE, "FALSE");
 	}
 
-	itoa(accel_values.z, z_ascii, 16); // convert axis data to ascii
-	itoa(accel_values.y, y_ascii, 16); // convert axis data to ascii
-	itoa(accel_values.x, x_ascii, 16); // convert axis data to ascii
+	// convert axis data to ascii
+	itoa(accel_values.z, z_ascii, 16);
+	itoa(accel_values.y, y_ascii, 16);
+	itoa(accel_values.x, x_ascii, 16);
 
+	// populate axis data lines
 	snprintf(z_axis, MSG_LEN,"%sZ-AXIS: %s\r\n", ANSI_CLEAR_LINE, z_ascii);
 	snprintf(y_axis, MSG_LEN,"%sY-AXIS: %s\r\n", ANSI_CLEAR_LINE, y_ascii);
 	snprintf(x_axis, MSG_LEN,"%sX-AXIS: %s\r\n", ANSI_CLEAR_LINE, x_ascii);
 
-	// move cursor to status line, rewrite status line, restore cursor
-	snprintf(message, MSG_LEN, "%s%s%s%s%s%s%s%s%s", ANSI_SAVE_CURSOR_POS, ANSI_MOVE_TO_STATUS_LINE,
-			INTRO_MSG, GIT_COMMIT, level, x_axis, y_axis, z_axis, ANSI_RETURN_CURSOR_POS);
+	// populate each ANSI char and data sequence
+	snprintf(move_to_status, MSG_LEN, "%s%s%s%s",
+			ANSI_SAVE_CURSOR_POS,
+			ANSI_HIDE_CURSOR,
+			ANSI_MOVE_TO_STATUS_LINE,
+			INTRO_MSG);
+	snprintf(data, MSG_LEN, "%s%s%s%s",
+			level,
+			x_axis,
+			y_axis,
+			z_axis);
+	snprintf(move_to_cmd, MSG_LEN, "%s%s",
+			ANSI_RETURN_CURSOR_POS,
+			ANSI_SHOW_CURSOR);
 
-	HAL_UART_Transmit(huart, (uint8_t*)message, strlen(message), TIMEOUT);
+	// combine status line rewrite steps into a single message
+	snprintf(message, MSG_LEN, "%s%s%s",
+			move_to_status,
+			data, move_to_cmd);
+
+	HAL_UART_Transmit(huart, (uint8_t*)move_to_status, strlen(move_to_status), TIMEOUT);
+	HAL_UART_Transmit(huart, (uint8_t*)data, strlen(data), TIMEOUT);
+	HAL_UART_Transmit(huart, (uint8_t*)move_to_cmd, strlen(move_to_cmd), TIMEOUT);
+	//HAL_UART_Transmit(huart, (uint8_t*)message, strlen(message), TIMEOUT);
 
     return;
 }
