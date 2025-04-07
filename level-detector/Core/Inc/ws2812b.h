51a00edb7765f0e83a50ed9254366057137e5405
Jacob Meyer <meyer28j@uregina.ca>
2025-04-07 16:25:01 -0600

    Add .c and .h template for ws2812b led matrix

---
+++ level-detector/Core/Inc/ws2812b.h
@@ -0,0 +1,15 @@
+/**
+ * @file ws2812b.h
+ * @author Jacob Meyer
+ * @brief This file contains the implementation of PWM DMA functions for the WS2812B LED Matrix.
+ *
+ *
+ */
+
+#ifndef INC_WS2812B_H_
+#define INC_WS2812B_H_
+
+
+
+
+#endif /* INC_WS2812B_H_ */
---
+++ level-detector/Core/Src/ws2812b.c
@@ -0,0 +1,8 @@
+/*
+ * ws2812b.c
+ *
+ *  Created on: Apr 7, 2025
+ *      Author: jacob
+ */
+
+
