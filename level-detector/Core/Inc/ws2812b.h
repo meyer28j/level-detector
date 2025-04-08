4522ea450e4d49493171f3e49791ba9a8b090d55
Jacob Meyer <meyer28j@uregina.ca>
2025-04-07 20:12:45 -0600

    Addd ws2812b functions for initializing, updating, and setting each pixel's data for the matrix

--- level-detector/Core/Inc/ws2812b.h
+++ level-detector/Core/Inc/ws2812b.h
@@ -9,7 +9,27 @@
 #ifndef INC_WS2812B_H_
 #define INC_WS2812B_H_
 
+#include "stm32f1xx_hal.h"
 
+// defined in main.c by HAL library and .ioc file generation
+extern TIM_HandleTypeDef htim3;
+extern DMA_HandleTypeDef hdma_tim3_ch1_trig;
 
+#define WS2812B_BIT_0_HIGH	26	// approx 0.4us
+#define WS2812B_BIT_0_LOW	54	// approx 0.8us
+#define WS2812B_BIT_1_HIGH	51	// approx 0.85us
+#define WS2812B_BIT_1_LOW	29	// approx 0.45us
+#define WS2812B_RESET 		79	// full ARR length
+
+#define NUM_LEDS 64
+#define BITS_PER_TRANSFER 24
+
+extern uint16_t led_pwm_data[NUM_LEDS][BITS_PER_TRANSFER];
+
+void WS2812B_init();
+
+void WS2812B_update();
+
+void WS2812B_set_pixel_color(uint16_t led_index, uint8_t g, uint8_t r, uint8_t b);
 
 #endif /* INC_WS2812B_H_ */
--- level-detector/Core/Src/ws2812b.c
+++ level-detector/Core/Src/ws2812b.c
@@ -5,4 +5,42 @@
  *      Author: jacob
  */
 
+#include "ws2812b.h"
 
+uint16_t led_pwm_data[NUM_LEDS][BITS_PER_TRANSFER];
+
+void WS2812B_init()
+{
+	for (int i = 0; i < NUM_LEDS; i++)
+	{
+		// turn off all leds
+		WS2812B_set_pixel_color(i, 0, 0, 0);
+	}
+
+	WS2812B_update();
+}
+
+void WS2812B_update()
+{
+	HAL_TIM_PWM_Start_DMA(&htim3, TIM_CHANNEL_1, (uint32_t*)led_pwm_data, (uint16_t)(NUM_LEDS * BITS_PER_TRANSFER));
+	HAL_Delay(200);
+	HAL_TIM_PWM_Stop_DMA(&htim3, TIM_CHANNEL_1);
+	HAL_Delay(200);
+
+void WS2812B_set_pixel_color(uint16_t led_index, uint8_t g, uint8_t r, uint8_t b)
+{
+	// extract each high and low bit from the 24-bit data series for each pixel's color
+	// data sent in MSB first as [8 bits GREEN][8 bits RED][8 bits BLUE]
+	for (int i = 0; i < 8; i++)
+	{
+		led_pwm_data[led_index][i] = (g & (1 << (7 - i))) ? WS2812B_BIT_1_HIGH : WS2812B_BIT_0_HIGH;
+	}
+	for (int i = 7; i >= 0; i--)
+	{
+		led_pwm_data[led_index][i + 8] = (r & (1 << (7 - i))) ? WS2812B_BIT_1_HIGH : WS2812B_BIT_0_HIGH;
+	}
+	for (int i = 7; i >= 0; i--)
+	{
+		led_pwm_data[led_index][i + 16] = (b & (1 << (7 - i))) ? WS2812B_BIT_1_HIGH : WS2812B_BIT_0_HIGH;
+	}
+}
